69ec745a44414b8d9d580bad0b215e3c68543df2
Sukrut <dekhanesd7@gmail.com>
2024-01-30 22:27:33 +0530

    tried flex on login page

--- src/Components/LoginForm/Credentials.jsx
+++
@@ -1,6 +0,0 @@
-const credentials = {
-    email: "dekhanesd7@gmail.com",
-    password: "123"
-}
-
-export default credentials
--- src/Components/LoginForm/LoginForm.jsx
+++ src/Components/LoginForm/LoginForm.jsx
@@ -3,19 +3,40 @@ import { useNavigate } from "react-router-dom";
 import Input from "../Input";
 
 function LoginForm() {
-    const navigate = useNavigate();
-    return <div class="container">
-        <form onSubmit={() => {
-            navigate('/HomePage')
-        }}>
-            <div class="container">
-                <h2>Login here</h2>
-                <Input label="Email" for="email" type="email" placeholder="Enter email" name="email" required />
-                <Input label="Password" for="password" type="password" placeholder="Enter password" name="password" required />
-                <button type="submit">Login</button>
-            </div>
-        </form>
+  const navigate = useNavigate();
+  return (
+    <div class="container">
+      <form
+        className="loginPage"
+        onSubmit={() => {
+          navigate("/HomePage");
+        }}
+      >
+        <h2>Login here</h2>
+        <div className="emailContainer">
+          <Input
+            label="Email"
+            for="email"
+            type="email"
+            placeholder="Enter email"
+            name="email"
+            required
+          />
+        </div>
+        <div className="passwordContainer">
+          <Input
+            label="Password"
+            for="password"
+            type="password"
+            placeholder="Enter password"
+            name="password"
+            required
+          />
+        </div>
+        <button type="submit">Login</button>
+      </form>
     </div>
+  );
 }
 
 export default LoginForm;
